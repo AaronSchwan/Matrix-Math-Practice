ab43340a76aea58ed68a02287aee023e6ae14cbe
Aaron Schwan <43838067+AaronSchwan@users.noreply.github.com>
2020-08-15 07:08:01 -0700

    started

---
+++ algebraicMatrixMath.py
@@ -0,0 +1,13 @@
+#default imports
+import math
+import statistics
+
+#non-default imports
+import pandas as pd
+import numpy as np
+
+class Matrix:
+
+    def __init__(self,data:pd.DataFrame):
+        pass;
+    
